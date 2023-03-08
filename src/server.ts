631e2edb5dbeec062c5136e032939639bf3209fa
Adarsh <kpadarsh6801@gmail.com>
2023-03-08 17:38:04 +0530

    login api created

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import { sample_foods, sample_tags } from './data';
-
+import { sample_foods, sample_tags, sample_users } from './data';
+import jwt from "jsonwebtoken"
+import * as dotenv from "dotenv"
+dotenv.config({path:__dirname+'/.env'});
 
 const app=express();
+app.use(express.json())
 app.use(cors({
     credentials:true,
     origin:["http://localhost:4200"]
@@ -36,6 +39,29 @@ app.get('/api/foods/:foodId',(req,res)=>{
     const food=sample_foods.find(food=>food.id==foodId);
     res.send(food)
 })
+
+
+app.post('/api/users/login',(req,res)=>{
+    const body=req.body;
+    const {email,password}=req.body;
+    const user=sample_users.find(user=>user.email===email &&
+        user.password===password);
+        if(user){
+            res.send(generateTokenResponse(user));
+        }else{
+            res.status(400).send("User name or password is not valid!")
+        }
+})
+
+const generateTokenResponse=(user:any)=>{
+    const token =jwt.sign({
+        email:user.email,isAdmin:user.isAdmin
+    },"process.env.JWT_SECRET_TOKEN",{
+        expiresIn:'30d'
+    })
+    user.token=token;
+    return user;
+}
 
 const PORT=5000;
 app.listen(PORT,()=>{
